9c6ae9a71db7687414febe1fadf7ef4d10d6ead8
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-06-12 17:23:05 +0530

    Implement match manager and game flow

--- game-engine/Main.java
+++ game-engine/Main.java
@@ -3,8 +3,32 @@ public class Main {
     public static void main(String[] args) {
 
         Player p1 =
-                new Player("Player1", "5831");
+                new Player(
+                        "Player1",
+                        "5831"
+                );
 
-        System.out.println(p1);
+        Player p2 =
+                new Player(
+                        "Player2",
+                        "7204"
+                );
+
+        MatchManager match =
+                new MatchManager(
+                        p1,
+                        p2
+                );
+
+        GuessResult result =
+                match.submitGuess("7204");
+
+        System.out.println(result);
+
+        System.out.println(
+                match.getWinner()
+        );
     }
+
+    
 }
---
+++ game-engine/MatchManager.java
@@ -0,0 +1,90 @@
+public class MatchManager {
+
+    private Player player1;
+    private Player player2;
+
+    private GameState gameState;
+    private GameEngine gameEngine;
+
+    private Player winner;
+
+    public MatchManager(Player player1,
+                        Player player2) {
+
+        this.player1 = player1;
+        this.player2 = player2;
+
+        this.gameState = new GameState();
+        this.gameEngine = new GameEngine();
+    }
+
+    public Player getCurrentPlayer() {
+
+        if (gameState.isPlayer1Turn()) {
+            return player1;
+        }
+
+        return player2;
+    }
+
+    public Player getOpponent() {
+
+        if (gameState.isPlayer1Turn()) {
+            return player2;
+        }
+
+        return player1;
+    }
+
+    public GameState getGameState() {
+        return gameState;
+    }
+
+    public Player getWinner() {
+        return winner;
+    }
+
+    public GuessResult submitGuess(String guess) {
+
+    Player currentPlayer = getCurrentPlayer();
+
+    Player opponent = getOpponent();
+
+    GuessResult result =
+            gameEngine.checkGuess(
+                    opponent.getSecretNumber(),
+                    guess
+            );
+
+    GuessRecord record =
+            new GuessRecord(
+                    currentPlayer.getName(),
+                    guess,
+                    result.getBulls(),
+                    result.getCows()
+            );
+
+    gameState.addRecord(record);
+
+    if (gameEngine.isWinner(result)) {
+
+        winner = currentPlayer;
+
+        gameState.setGameOver(true);
+    }
+    else {
+
+        gameState.switchTurn();
+    }
+
+    return result;
+}
+
+public boolean isGameOver() {
+    return gameState.isGameOver();
+}
+
+public boolean isPlayer1Turn() {
+    return gameState.isPlayer1Turn();
+}
+}
